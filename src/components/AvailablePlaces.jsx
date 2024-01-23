c475157460d6b4ec6255a38dd705e908ca7958c6
Gitros <31899790+Gitros@users.noreply.github.com>
2024-01-23 16:07:34 +0100

    http errors

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,26 +1,43 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState();
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    fetch('http://localhost:5173/places')
-    .then((response) => {
-      return response.json();
-    }).then((resData) => {
-      setAvailablePlaces(resData.places);
-    })
-  }, [])
+    async function fetchPlaces() {
+      setIsFetching(true);
 
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
 
+        if (!response.ok) {
+          throw new Error('failed to fetch places');
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError(error);
+      }
+      setIsFetching(false);
+    }
+    fetchPlaces();
+  }, []);
 
+  if (error) {
+    return <Error title="An error occured!" message={error.message} />
+  }
 
   return (
     <Places
       title="Available Places"
-      places={[availablePlaces]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetchnig place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
